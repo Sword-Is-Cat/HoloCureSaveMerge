e53838092f4477cda45e3ef79194008a22ac84df
jang <jhbsp@naver.com>
2023-02-11 11:23:24 +0900

    230211 v0.5 대응 및 rework

--- logic.js
+++ logic.js
@@ -2,350 +2,90 @@
  * 
  */
 let data;
+const fileName = 'save_n.dat';
 
-$(function() {
-
-	drawScreen();
-
-	addListener();
-
+document.addEventListener("DOMContentLoaded", () => {
+	initialize();
 });
 
-function addListener(){
-	
-	//세이브파일 첨부 시
-	$('input[type=file]').on('change', function() {
-		
-		var fr = new FileReader();
-		fr.onload = function() {
-			
+const initialize = () => {
+
+	const dropzone = document.getElementById('drop_zone');
+	dropzone.ondragover = (evt) => {
+		evt.preventDefault();
+	}
+	// 파일 드롭
+	dropzone.ondrop = (evt) => {
+		dropHandler(evt);
+	}
+
+	// 치트버튼
+	document.querySelectorAll('button[name=cheat]').forEach((btn) => {
+		btn.onclick = () => cheat(btn.id);
+	});
+
+	// 다운로드
+	document.getElementById('download').onclick = () => {
+		encodeAndDownload();
+	}
+}
+
+const dropHandler = (ev) => {
+	console.log('File(s) dropped');
+	// Prevent default behavior (Prevent file from being opened)
+	ev.preventDefault();
+
+	if (ev.dataTransfer.items) {
+		// Use DataTransferItemList interface to access the file(s)
+		[...ev.dataTransfer.items].forEach((item, i) => {
+			// If dropped items aren't files, reject them
+			if (item.kind === 'file') {
+				const file = item.getAsFile();
+				readFile(file);
+			}
+		});
+	} else {
+		// Use DataTransfer interface to access the file(s)
+		[...ev.dataTransfer.files].forEach((file, i) => {
+			readFile(file);
+		});
+	}
+}
+
+const readFile = (file) => {
+	if (file.name === fileName) {
+		let fr = new FileReader();
+		fr.onload = function () {
 			parseData(fr.result);
-			showData();
 		}
-		
-		fr.readAsText(this.files[0]);
-	});
-	
-	//다운로드버튼 클릭
-	$('#download').on('click', function() {
-		
-		update();
-		
-	});
-	
-	$('input[type=number]').attr('min', 0).attr('max', 999);
-	$('#holoCoins').attr('max', 10000000);
-	
-}
-
-function drawScreen() {
-
-	const $body = document.querySelector('tbody');
-
-	let $tr, $th, $td;
-	let $tr1, $tr2, $input;
-
-	// CODE LINE create
-	$tr = newChild('tr', $body);
-	
-	$th = newChild('th', $tr);
-	$th.innerText = 'CODE';
-
-	$td = newChild('td', $tr);
-	$td.setAttribute('colspan', 7);
-	$td.setAttribute('id', 'code');
-	// !CODE LINE create
-
-	// UPGRADE create
-	$tr = newChild('tr', $body);
-
-	$th = newChild('th', $tr);
-	$th.setAttribute('colspan', 8);
-	$th.innerText = 'UPGRADE';
-
-	for (let idx = 0; idx < keys.upg.length; idx++) {
-
-		let arr = keys.upg[idx];
-		let key = arr[0];
-		let cnt = arr[1];
-
-		if (idx % 8 == 0) {
-			$tr1 = newChild('tr', $body);
-			$tr2 = newChild('tr', $body);
-		}
-
-		$th = newChild('th', $tr1);
-		$td = newChild('td', $tr2);
-
-		$th.innerText = dic[key]||key;
-		if (key == 'holoCoins') {
-			$input = newChild('input', $td);
-			$input.setAttribute('type', 'number');
-		} else {
-			$input = newChild('select', $td);
-			let $option;
-			
-			for (let i = 0; i <= cnt; i++) {
-				$option = newChild('option', $input);
-				$option.innerText = i;
-				$option.value = i;
-			}
-
-		}
-
-		$input.setAttribute('id', key);
-
-		if (idx == keys.upg.length - 1) {
-			let width = (79 - idx) % 8 +1 ;
-			$th.setAttribute('colspan', width);
-			$td.setAttribute('colspan', width);
-		}
-
-	}
-	// !UPGRADE create
-
-	// CHARACTER create
-	$tr = newChild('tr', $body);
-	
-	$th = newChild('th', $tr);
-	$th.setAttribute('colspan', 8);
-	$th.innerText = 'CHARACTER';
-
-	drawNumber(keys.chara, $body);
-	// !CHARACTER create
-	
-	// TEAR create
-	$tr = newChild('tr', $body);
-	
-	$th = newChild('th', $tr);
-	$th.setAttribute('colspan', 8);
-	$th.innerText = 'TEARS';
-
-	drawNumber(keys.tear, $body);
-	// !TEAR create
-
-	// ITEMS create
-	$tr = newChild('tr', $body);
-	
-	$th = newChild('th', $tr);
-	$th.setAttribute('colspan', 8);
-	$th.innerText = 'UNLOCK ITEMS';
-	
-	drawCheckbox(keys.item, $body);
-	// !ITEMS create
-
-	// WEAPONES create
-	$tr = newChild('tr', $body);
-	
-	$th = newChild('th', $tr);
-	$th.setAttribute('colspan', 8);
-	$th.innerText = 'UNLOCK WEAPONES';
-
-	drawCheckbox(keys.weapone, $body);
-	// !WEAPONES create
-
-	// COLLAB create
-	$tr = newChild('tr', $body);
-	
-	$th = newChild('th', $tr);
-	$th.setAttribute('colspan', 8);
-	$th.innerText = 'UNLOCK COLLABS';
-	
-	drawCheckbox(keys.collab, $body);
-	// !COLLAB create
-	
-	// OUTFIT create
-	$tr = newChild('tr', $body);
-	
-	$th = newChild('th', $tr);
-	$th.setAttribute('colspan', 8);
-	$th.innerText = 'UNLOCK OUTFITS';
-
-	drawCheckbox(keys.outfit, $body);
-	// !OUTFIT create
-
-}
-
-
-function newChild(tagName, parent) {
-
-	let element = document.createElement(tagName);
-	if (parent)
-		parent.appendChild(element);
-	return element;
-
-}
-
-function newCheckbox(key, $td){
-	
-	let $label = newChild('label', $td);
-
-	let $input = newChild('input', $label);
-	$input.setAttribute('type', 'checkbox');
-	$input.setAttribute('name', 'item');
-	$input.setAttribute('value', key);
-	$input.setAttribute('id', key);
-
-	let $span = newChild('span', $label);
-	$span.innerText = dic[key]||key;
-	
-}
-
-function drawNumber(keyArr, $body){
-	
-	let $tr, $th, $td, $input;
-	
-	for (let idx = 0; idx < keyArr.length; idx++) {
-
-		let key = keyArr[idx];
-
-		if (idx % 4 == 0)
-			$tr = newChild('tr', $body);
-
-		$th = newChild('th', $tr);
-		$td = newChild('td', $tr);
-		$input = newChild('input', $td);
-
-		$th.innerText = dic[key]||key;
-		$input.setAttribute('type', 'number');
-		$input.setAttribute('id', key);
-		$input.value = 0;
-
-		if (idx == keyArr.length - 1) {
-			let cnt = ((79 - idx) % 4) * 2;
-			if(cnt>0){
-				let $empty = newChild('td', $tr);
-				$empty.setAttribute('colspan', cnt);
-			}
-		}
-
-	}
-	
-}
-
-function drawCheckbox(keyArr, $body){
-	
-	let $tr, $td;
-	
-	for(let idx = 0 ; idx<keyArr.length ; idx++){
-		
-		let key = keyArr[idx];
-		
-		if(idx%8==0)
-			$tr = newChild('tr', $body);
-		
-		$td = newChild('td', $tr);
-		
-		newCheckbox(key, $td);
-		
-		if (idx == keyArr.length - 1) {
-			let cnt = ((79 - idx) % 8) * 2;
-			if(cnt>0){
-				let $empty = newChild('td', $tr);
-				$empty.setAttribute('colspan', cnt);
-			}
-		}
-		
-	}
-	
-}
-
-function parseData(str) {
-
-	data = new Object();
-
-	data.uid = str.substring(0, 80);
-	data.spec = JSON.parse(atob(str.substring(80)).replaceAll('\x00', ''));
-
-}
-
-function showData() {
-
-	console.log(data);
-
-	$('#code').text(data.uid);
-	let spec = data.spec;
-
-	keys.upg.forEach(function(arr) {
-		let key = arr[0];
-		$('#' + key).val(data.spec[key]);
-	});
-
-	data.spec.characters.forEach(function(arr) {
-		let key = arr[0], lv = arr[1];
-		$('#' + key).val(lv);
-	});
-	
-	data.spec.tears.forEach(function(arr) {
-		let key = arr[0], lv = arr[1];
-		$('#' + key).val(lv);
-	});
-	
-	data.spec.seenCollabs.forEach(function(key){
-		$('#'+key).prop('checked', true);
-	});
-	
-	data.spec.unlockedItems.forEach(function(key){
-		$('#'+key).prop('checked', true);
-	});
-	
-	data.spec.unlockedWeapons.forEach(function(key){
-		$('#'+key).prop('checked', true);
-	});
-	
-	data.spec.unlockedOutfits.forEach(function(key){
-		$('#'+key).prop('checked', true);
-	});
-	
-	$('tbody').show();
-
-}
-
-function update(){
-	
-	if (data && data.uid) {
-
-		upgrades();
-		data.spec.unlockedItems = items();
-		data.spec.unlockedWeapons = weapones();
-		data.spec.seenCollabs = collabs();
-		data.spec.unlockedOutfits = outfits();
-		data.spec.characters = characters();
-		data.spec.tears = tears();
-		
-		encoding();
-
+		fr.readAsText(file);
 	} else {
-		alert('원본 세이브파일이 필요합니다');
+		print_message('[에러]:'+fileName + ' 파일을 첨부 해 주세요');
 	}
-	
-	
 }
 
-function encoding() {
-
-	if (data && data.uid) {
-
-		let encoded = data.uid
-				+ btoa(JSON.stringify(data.spec)).replaceAll('=', 'A');
-		download(encoded);
-
-	} else {
-		alert('원본 세이브파일이 필요합니다');
+const parseData = (str) => {
+	try{
+		data = JSON.parse(atob(str));
+		print_message('[성공]:세이브 파일 로드');
+	}catch(err){
+		print_message('[에러]:세이브 파일 로드 실패');
 	}
-
 }
 
-function download(encoded_string) {
+const encodeAndDownload = () => {
+	download(btoa(JSON.stringify(data)));
+}
+
+const download = (encoded_string) => {
 
 	if (encoded_string) {
 
-		var element = document.createElement('a');
-		element.setAttribute('href', 'data:text/plain;charset=utf-8,'
-				+ encodeURIComponent(encoded_string));
-		element.setAttribute('download', 'save.dat');
+		let element = document.createElement('a');
+		element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(encoded_string));
+		element.setAttribute('download', fileName);
 
-		element.style.display = 'none';
+		element.className = 'hide';
 
 		document.body.appendChild(element);
 		element.click();
@@ -354,192 +94,85 @@ function download(encoded_string) {
 	}
 }
 
-function items() {
-
-	let arr = [];
-
-	$('input:checkbox[name=item]:checked').each(function() {
-		arr.push(this.value);
-	});
-
-	return arr;
-
+const cheat = (key) => {
+	let text = cheat_action(key);
+	print_message(text);
 }
 
-function collabs() {
-
-	let arr = [];
-
-	$('input:checkbox[name=collab]:checked').each(function() {
-		arr.push(this.value);
-	});
-
-	return arr;
-
+const cheat_action = (key) => {
+	let text;
+	try {
+		if (data) {
+			switch (key) {
+				case 'ch_coin':
+					data.holoCoins += 1000000;
+					text = '[성공]:coin-cheat';
+					break;
+				case 'ch_tear':
+					data.tears.forEach((arr) => arr[1] += 100);
+					text = '[성공]:tear-cheat';
+					break;
+				case 'ch_chara':
+					data.characters.forEach((chara) => chara[1] += 10);
+					text = '[성공]:chara-cheat';
+					break;
+			}
+		}else{
+			text = '[에러]:세이브파일이 로드되지 않았습니다';
+		}
+	} catch (err) {
+		text = '[에러]:'+err.message;
+	}
+	return text;
 }
 
-function outfits() {
-
-	let arr = [];
-
-	$('input:checkbox[name=outfit]:checked').each(function() {
-		arr.push(this.value);
-	});
-
-	return arr;
-
-}
-
-function weapones() {
-
-	let arr = [];
-
-	$('input:checkbox[name=weapone]:checked').each(function() {
-		arr.push(this.value);
-	});
-
-	return arr;
-
-}
-
-function characters(){
-	
-	let arr = [];
-	let temp;
-	keys.chara.forEach(function(key){
-		
-		temp = new Array();
-		temp.push(key);
-		temp.push((Number)($('#'+key).val()));
-		arr.push(temp);
-		
-	});
-	
-	return arr;
-	
-}
-
-function tears(){
-	
-	let arr = [];
-	let temp;
-	keys.tear.forEach(function(key){
-		
-		temp = new Array();
-		temp.push(key);
-		temp.push((Number)($('#'+key).val()));
-		arr.push(temp);
-		
-	});
-	
-	return arr;
-	
-}
-
-function upgrades(){
-	
-	keys.upg.forEach(function(arr){
-		let key = arr[0];
-		data.spec[key] = (Number)($('#'+key).val());
-		
-	});
-	
+const print_message = (text) => {
+	let span = document.createElement('span');
+	span.innerText = text;
+	document.getElementById('message_zone').appendChild(span);
 }
 
 const keys = {
 
-	'upg' : [ 
-		['HP',10],
-		['ATK',10],
-		['SPD',10],
-		['crit',5],
-		['pickupRange',10],
-		['haste',5],
-		['regen',5],
-		['specUnlock',1],
-		['specCDR',5],
-		['growth',1],
-		['EXP',5],
-		['food',5],
-		['moneyGain',10],
-		['reroll',5],
-		['enhanceUp',5],
-		['DR',5],
-		['holoCoins'] ],
-	'chara' : 
-		[ 'ame', 'gura', 'ina', 'kiara', 'calli', 
-			'bae', 'kronii', 'fauna', 'mumei', 'sana', 
-			'irys', 'fubuki', 'mio', 'okayu', 'korone', 
-			'sora', 'azki', 'roboco', 'suisei', 'miko' ],
-	'weapone' :[ 
-		'SpiderCooking', 'EliteLava', 'CEOTears', 'WamyWater','HoloBomb', 'PsychoAxe', 'BLBook', 
-		'CuttingBoard', 'HoloLaser', 'Tailplug', 'Glowstick', 'XPotato', 'IdolSong' 
-		],
-	'item' :[ 
-		'Sake', 
-		'Limiter', 'BodyPillow', 'InjectionAsacoco', 'CreditCard', 'Halu','HolyMilk', 'FaceMask', 
+	'upg': [
+		['HP', 10],
+		['ATK', 10],
+		['SPD', 10],
+		['crit', 5],
+		['pickupRange', 10],
+		['haste', 5],
+		['regen', 5],
+		['specUnlock', 1],
+		['specCDR', 5],
+		['growth', 1],
+		['EXP', 5],
+		['food', 5],
+		['moneyGain', 10],
+		['reroll', 5],
+		['enhanceUp', 5],
+		['DR', 5],
+		['holoCoins']],
+	'chara':
+		['ame', 'gura', 'ina', 'kiara', 'calli',
+			'bae', 'kronii', 'fauna', 'mumei', 'sana',
+			'irys', 'fubuki', 'mio', 'okayu', 'korone',
+			'sora', 'azki', 'roboco', 'suisei', 'miko'],
+	'weapone': [
+		'SpiderCooking', 'EliteLava', 'CEOTears', 'WamyWater', 'HoloBomb', 'PsychoAxe', 'BLBook',
+		'CuttingBoard', 'HoloLaser', 'Tailplug', 'Glowstick', 'XPotato', 'IdolSong'
+	],
+	'item': [
+		'Sake',
+		'Limiter', 'BodyPillow', 'InjectionAsacoco', 'CreditCard', 'Halu', 'HolyMilk', 'FaceMask',
 		'Plushie', 'SuccubusHorn', 'FullMeal', 'Membership', 'PikiPikiPiman', 'Headphones', 'ChickensFeather',
 		'GorillasPaw', 'UberSheep', 'IdolCostume', 'EnergyDrink', 'PiggyBank', 'StudyGlasses', 'Bandaid',
-		'GWSPill','SuperChattoTime'
-		],
-	'collab' :[ 
-		'RapDog', 'BreatheInAsacoco', 'DragonBeam', 'BrokenDreams', 'EliteCooking', 
-		'IdolConcert', 'StreamOfTears', 'BLLover', 'MiComet', 'MariLamy', 'FlatBoard', 'LightBeam' 
-		],
-	'outfit' :
-		[ 'kurokami', 'ameAlt1', 'inaAlt1', 'guraAlt1', 'calliAlt1', 'kiaraAlt1', 'irysAlt1', 'baeAlt1', 'sanaAlt1', 'faunaAlt1', 'mumeiAlt1', 'kroniiAlt1'],
-	'tear':
-		['myth', 'councilHope', 'gamers', 'gen0']
+		'GWSPill', 'SuperChattoTime'
+	],
+	'collab': [
+		'RapDog', 'BreatheInAsacoco', 'DragonBeam', 'BrokenDreams', 'EliteCooking',
+		'IdolConcert', 'StreamOfTears', 'BLLover', 'MiComet', 'MariLamy', 'FlatBoard', 'LightBeam'
+	],
+	'outfit':
+		['kurokami', 'ameAlt1', 'inaAlt1', 'guraAlt1', 'calliAlt1', 'kiaraAlt1', 'irysAlt1', 'baeAlt1', 'sanaAlt1', 'faunaAlt1', 'mumeiAlt1', 'kroniiAlt1'],
 
 };
-
-const dic = {
-		// UPG
-		'pickupRange':'획득반경',
-		'specUnlock':'스킬해금',
-		'specCDR':'스킬쿨감',
-		'growth':'성장',
-		'reroll':'리롤',
-		'enhanceUp':'강화확률',
-		'DR':'방어',
-		// CHARACTER
-		'ame':'아메',
-		'gura':'구라',
-		'ina':'이나',
-		'kiara':'키아라',
-		'calli':'칼리',
-		'bae':'밸즈',
-		// TEAMNAME
-		'myth':'MYTH',
-		'councilHope':'카운슬(irys)',
-		'gamers':'게이머즈',
-		'gen0':'0기생',
-		// ITEM
-		'BodyPillow':'다키마쿠라',
-		'InjectionAsacoco':'주입식아사코코',
-		'PiggyBank':'돼지저금통',
-		'SuperChattoTime':'슈퍼챗타임',
-		// WEAPONE
-		'CEOTears':'YAGOO의눈물',
-		'CuttingBoard':'도마',
-		'HoloLaser':'팬빔',
-		'Tailplug':'삽입식아사코코',
-		// COLLAB
-		'BreatheInAsacoco':'흡입식아사코코',
-		'MariLamy':'얼어붙은바다',
-		// OUTFIT
-		'kurokami':'후부키(쿠로)',
-		'ameAlt1':'아메',
-		'inaAlt1':'이나',
-		'guraAlt1':'구라',
-		'calliAlt1':'칼리',
-		'kiaraAlt1':'키아라',
-		'irysAlt1':'아이리스',
-		'baeAlt1':'밸즈',
-		'sanaAlt1':'사나',
-		'faunaAlt1':'파우나',
-		'mumeiAlt1':'무메이',
-		'kroniiAlt1':'크로니',
-		// END
-		'LASTLINE':'LASTLINE'
-}
